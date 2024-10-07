f3038b956aaa379a40e2a9809b5ad7ec34590f73
Akshay Moharir <moharir.akshay@gmail.com>
2024-10-06 23:09:15 -0400

    practice multiple variable linear regression per coursera course Supervised ML week-2.

--- multiple-variable-linear-regression/multiple-variable-linear-regression.py
+++ multiple-variable-linear-regression/multiple-variable-linear-regression.py
@@ -18,15 +18,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Training data
-X_train = np.array([[2104, 5, 45, 1], [1416, 3, 40, 2], [852, 2, 35, 1]])
+X_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
 Y_train = np.array([460, 232, 178])
 
-print(f"Shape of X_train:{X_train[0].shape}")
+print(f"Shape of X_train:{X_train.shape}")
+print(f"Shape of Y_train:{Y_train.shape}")
 
 def initialize_weights_and_biases(X_train):
     W = np.ones((X_train[0].shape)) * 0.000001
     b = np.array([1.0]) * 0.000001
-    print(f"Number of Input features:{X_train[0].shape}, Shape of W:(n_x, n_h):{X_train[0].shape, 1} ")
+    W = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
+    b = np.array([785.1811367994083])
+    W = np.zeros_like(W)
+    b = np.array([0.])
+    print(f"Shape of W:{W.shape} \nShape of b:{b.shape}")
     return W, b
 
 # Compute Cost
@@ -37,55 +42,69 @@ def compute_cost(X_train, Y_train, W, b, m):
     cost = 0
     f_wb = 0
     for i in range(m):
-        f_wb = np.dot(W.T,X_train[i]) + b
+        f_wb = np.dot(W,X_train[i]) + b
         cost = cost + (f_wb - Y_train[i])**2
-    total_cost = ((1/(2*m))*cost).astype(float)
-    print(total_cost)
+    total_cost = ((1/(2*m))*cost)
+    #print(total_cost)
     return total_cost
 
 # Compute Gradient
 # Partial derivative of cost wrt W is (1/2m)* summation(2(WX[i]+b - Y[i])X[i])
 # Partial derivative of cost wrt b is (1/2m)* summation(2(WX[i]+b - Y[i]))
 def compute_gradient(X_train, Y_train, W, b, m):
-    dJ_dW_i = 0
+    n = X_train.shape[1]
+    dJ_dW_i = np.zeros(n)
     dJ_db_i = 0
     # Find the summation of the partial derivate of cost
     for i in range(m):
         f_wb = np.dot(W.T,X_train[i]) + b
-        dJ_dW_i += 2*(f_wb - Y_train[i])*X_train[i]
+        for j in range(n):
+            dJ_dW_i[j] += 2*(f_wb - Y_train[i])*X_train[i,j]
         dJ_db_i += 2*(f_wb - Y_train[i])
     # Divide the summation by 2m
     dJ_dW = (1/(2*m))*dJ_dW_i
+
+    
     dJ_db = (1/(2*m))*dJ_db_i
-    return dJ_dW.astype(float), dJ_db.astype(float)
+    return dJ_dW, dJ_db
 
 
 # Iterate training process
 m = X_train.shape[0]
 print(f"Number of training examples:{m}")
+number_of_features = X_train.shape[1]
+print(f"Number of features per example:{number_of_features}")
 
 # Number of iterations to train model on training data
-num_iterations = 500
+num_iterations = 1000
 
 # Learning rate
-learning_rate = 1e-8
+learning_rate = 5.0e-7
 
 # Initialize weights and biases
 W, b = initialize_weights_and_biases(X_train=X_train)
 
 # Iterate training
 history_of_cost = np.zeros((num_iterations))
-history_of_W1 = []
+history_of_W = np.zeros((num_iterations,W.shape[0]))
+history_of_b = np.zeros((num_iterations))
 for n in range(num_iterations):
     # Compute cost
     total_cost = compute_cost(X_train=X_train, Y_train=Y_train, W=W, b=b, m=m)
     history_of_cost[n] = total_cost
+    if n%100==0:
+        print(f"Iteration:{n}, cost:{total_cost}")
     # Compute gradient
     dJ_dW, dJ_db = compute_gradient(X_train=X_train, Y_train=Y_train, W=W, b=b, m=m)
     # Update weights and biases
     W = W - learning_rate*dJ_dW
     b = b - learning_rate*dJ_db
+    history_of_W[n] = W
+    history_of_b[n] = b
 
+print(f"Optimal cost:{history_of_cost[-1]:.4f}")
+print(f"Final W:{history_of_W[-1]}")
+print(f"Final b:{history_of_b[-1]:.3f}")
 
 # Plot
 fig = plt.figure()
